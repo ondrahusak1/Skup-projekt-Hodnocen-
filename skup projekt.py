5f7ad6606a62a077703e90868cb3c8ae7e2f6100
ondrahusak1 <121938160+ondrahusak1@users.noreply.github.com>
2023-06-13 09:03:01 +0200

    Add files via upload
    Ted jsem přidal vlastnosti a naše smajlíky na tlačítka

---
+++ skup projekt.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QLabel 
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'hodnocení'
+        self.left = 20
+        self.top = 20
+        self.width = 850
+        self.height = 500
+        self.initUI()
+        self.a=0
+        self.b=0
+        self.c=0
+        self.d=0
+        self.g =":)"
+        self.q =":]"
+        self.f =":/"
+        self.r =":("
+        self.setStyleSheet("background-color : lightblue")
+
